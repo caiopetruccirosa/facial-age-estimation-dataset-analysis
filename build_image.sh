33edd1b58dc7b311d5065a0ee46a7611a394d471
Caio Petrucci Rosa <caiopetruccirosa@gmail.com>
2024-06-23 19:40:49 -0300

    remove docker stuff

--- build_image.sh
+++
@@ -1,1 +0,0 @@
-docker build -t facial-age-estimation-dataset-analysis .
--- run_container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Notebooks path: $1"
-echo "Datasets path: $2"
-
-exec docker run -p 4321:4321 -v "$1":/workspace/notebooks -v "$2":/workspace/datasets -t facial-age-estimation-dataset-analysis
